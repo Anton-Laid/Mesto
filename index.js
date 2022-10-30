f642a686505ac6ee6d03b96b42835b5babfb75e4
Fra1zed <antonlaid@yandex.ru>
2022-10-30 11:34:27 +0300

    'feat: добавление js'

---
+++ index.js
@@ -0,0 +1,6 @@
+let open = document.querySelector('.profile__rectangle');
+let popup = document.querySelector('.');
+let close = document.querySelector('.');
+let formElement = document.querySelector('.');
+let Input = document.querySelector('.');
+let value = document.querySelector('.');
