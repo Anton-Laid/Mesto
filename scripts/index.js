518d9256c7c6bc439cfd6944586343842dc606d7
Fra1zed <antonlaid@yandex.ru>
2022-11-01 20:44:31 +0300

    На проверочку))

--- scripts/index.js
+++ scripts/index.js
@@ -5,27 +5,27 @@ const popupCloseElem = popupElem.querySelector('.popup__buttom-close');
 
 // Переменные для pupup
 
-let contantElement = document.querySelector('.popup__contant');
+let contantElement = document.querySelector('.popup__form');
 let buttom = document.querySelector('.popup__button');
 let nameInput = document.querySelector('.popup__type-name');
 let jobInput = document.querySelector('.popup__type-job');
 let title = document.querySelector('.profile__title');
-let subtitl = document.querySelector('.profile__subtitl');
+let subtitl = document.querySelector('.profile__subtitle');
 
 //  input
 
 aboutElem.addEventListener('click', () => {
-    popupElem.classList.add('popup__opened');
+    popupElem.classList.add('popup_opened');
 });
 
 popupCloseElem.addEventListener('click', () => {
-    popupElem.classList.remove('popup__opened');
+    popupElem.classList.remove('popup_opened');
 });
 
 
 popupElem.addEventListener('click', (event) => {
     if (event.target === event.currentTarget) {
-        popupElem.classList.remove('popup__opened');
+        popupElem.classList.remove('popup_opened');
     }
 })
 
@@ -41,5 +41,5 @@ function formSubmitHandler(evt) {
 contantElement.addEventListener('submit', formSubmitHandler);
 
 buttom.addEventListener('click', () => {
-    popupElem.classList.remove('popup__opened');
+    popupElem.classList.remove('popup_opened');
 });
